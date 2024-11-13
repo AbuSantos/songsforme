1da440105f969d667d50903b8406f154c6cfcb49
Abusantos <abusomwansantos@gmail.com>
2024-11-13 20:28:35 +0100

    added

--- components/dashboard/addnft/singles/single-music.tsx
+++ components/dashboard/addnft/singles/single-music.tsx
@@ -4,17 +4,11 @@ import { revalidateTag } from "next/cache";
 import { AllMySingle } from "./all-singles";
 
 // Fetch singles based on the userId (wallet address)
-const SingleMusic = async ({ userId }: { userId: string }) => {
-    // Fetch singles where owner matches the userId (wallet address)
-    const singles = await db.single.findMany({
-        where: {
-            owner: userId
-        },
-    });
+const SingleMusic = () => {
 
-    revalidateTag("single")
 
     // Pass the filtered singles to the client component
-    return <AllMySingle singles={singles} />;
+    //@ts-ignore
+    return <AllMySingle />;
 };
 export default SingleMusic
