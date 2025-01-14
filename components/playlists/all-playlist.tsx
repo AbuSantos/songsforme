c4ce9f0edbfc682c8f523d58e6ae837ad2852114
Abusantos <abusomwansantos@gmail.com>
2025-01-14 08:29:26 +0100

    added

--- components/playlists/all-playlist.tsx
+++ components/playlists/all-playlist.tsx
@@ -14,8 +14,7 @@ export const AllPlaylist = () => {
     const searchParams = useSearchParams();
     const userId = useRecoilValue(isConnected)?.userId;
 
-    try {
-           // Retrieve filters from the search params
+    // Retrieve filters from the search params
     const filter = searchParams.get("filter");
 
     const apiUrl = `/api/playlists?${new URLSearchParams({
@@ -24,49 +23,43 @@ export const AllPlaylist = () => {
 
     const { data: playlists, error, isLoading } = useSWR(apiUrl, fetcher);
 
-    if (error) return <div>Failed to load playlists</div>;
+    try {
+        if (error) return <div>Failed to load playlists</div>;
 
+        if (isLoading) {
+            return (
+                <div className="flex flex-col space-y-3">
+                    <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                    <div className="space-y-2">
+                        <Skeleton className="h-4 w-[250px]" />
+                        <Skeleton className="h-4 w-[200px]" />
+                    </div>
+                </div>
+            );
+        }
 
-
-    if (isLoading) {
         return (
-            <div className="flex flex-col space-y-3">
-                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-                <div className="space-y-2">
-                    <Skeleton className="h-4 w-[250px]" />
-                    <Skeleton className="h-4 w-[200px]" />
+            <div>
+                <div className="md:hidden mt-4">
+                    {
+                        playlists &&
+                        <MyPlaylist data={playlists} userId={userId} filter={filter} />
+                    }
+                </div>
+
+                <div className="hidden md:flex flex-wrap space-x-2 pb-4 mt-4">
+                    {playlists && playlists?.map((playlist: Playlist) => (
+                        <TrendingPlaylist
+                            album={playlist}
+                            key={playlist.id}
+                            className="w-[180px]"
+                        />
+                    ))}
                 </div>
             </div>
         );
-    }
 
-    return (
-        <div>
-            <div className="md:hidden mt-4">
-                {
-                    playlists &&
-                    <MyPlaylist data={playlists} userId={userId} filter={filter} />
-                }
-            </div>
-
-            <div className="hidden md:flex flex-wrap space-x-2 pb-4 mt-4">
-                {playlists && playlists?.map((playlist: Playlist) => (
-                    <TrendingPlaylist
-                        album={playlist}
-                        key={playlist.id}
-                        className="w-[180px]"
-                    />
-                ))}
-            </div>
-        </div>
-    );
-
-    } catch (error:any) {
+    } catch (error: any) {
         console.log(error.message);
-        
     }
-
- 
-
-
 };
