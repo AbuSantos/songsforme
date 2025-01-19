13db6d7e9a7e3863405e9a66cd6864bb7b253afc
Abusantos <abusomwansantos@gmail.com>
2025-01-19 17:34:45 +0100

    updated the load time for the images

--- components/homepage/homepage-carousel.tsx
+++ components/homepage/homepage-carousel.tsx
@@ -3,12 +3,30 @@ import Image from 'next/image'
 export const Carousel = () => {
     return (
         <div className="absolute bottom-24 flex flex-col items-center w-full">
-            <div className="relative w-full  h-10">
+            <div className="relative w-full h-10">
                 <div className="relative z-10 w-full">
-                    <Image src="/images/bg.jpg" alt="Artwork 1" width={1200} height={400} className="rounded-2xl shadow-lg w-full object-cover" />
+                    <Image
+                        src="/images/bg.jpg"
+                        alt="Artwork 1"
+                        width={1200}
+                        height={400}
+                        className="rounded-2xl shadow-lg w-full object-cover"
+                        priority
+                        placeholder="blur"
+                        blurDataURL="/images/bg-blur.jpg"
+                    />
                 </div>
                 <div className="absolute top-12 left-0 w-full z-20">
-                    <Image src="/images/bg-image.jpg" alt="Artwork 2" width={1200} height={400} className="rounded-2xl shadow-lg w-full object-cover" />
+                    <Image
+                        src="/images/bg-image.jpg"
+                        alt="Artwork 2"
+                        width={1200}
+                        height={400}
+                        className="rounded-2xl shadow-lg w-full object-cover"
+                        priority
+                        placeholder="blur"
+                        blurDataURL="/images/bg-image-blur.jpg"
+                    />
                 </div>
             </div>
         </div>
