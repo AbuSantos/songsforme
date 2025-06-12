c7476cc212765e9f456bcb35f7013920080788a2
Abusantos <abusomwansantos@gmail.com>
2025-06-12 04:21:24 +0100

    added

--- components/chart/ranked-song.ts
+++ components/chart/ranked-song.ts
@@ -4,7 +4,7 @@ import { db } from "@/lib/db";
 import { ListedNFT } from "@/types";
 
 type PriceDatType = {
-  timestamp: string; // Assuming ISO string from DB
+  timestamp: string;
   price: number;
 };
 
@@ -26,11 +26,13 @@ export const rankedSong = async (page: number = 1, limit: number = 20) => {
       price: true,
       contractAddress: true,
       priceData: true,
-      accumulatedTime: true, // Include this for filtering
+      accumulatedTime: true,
     },
-    take: limit,
+    // take: limit,
     skip: offset,
   });
+
+  console.log("Listed Data:", listedData);
 
   const now = new Date();
   const today = now.toISOString().slice(0, 10);
@@ -39,17 +41,20 @@ export const rankedSong = async (page: number = 1, limit: number = 20) => {
   tomorrowStart.setDate(todayStart.getDate() + 1);
 
   // Filter songs with sufficient play time
-  
+
   //CHANGE THE PLAYED TIME TO 8000
   const highPlayedSongs = listedData.filter(
     (song) =>
-      song?.accumulatedTime !== null &&
-      song?.accumulatedTime !== undefined &&
-      song.accumulatedTime >= 8000
+      (song?.accumulatedTime !== null &&
+        song?.accumulatedTime !== undefined &&
+        song.accumulatedTime >= 8000)
   );
+
   if (highPlayedSongs.length === 0) {
     return [];
   }
+
+  console.log("High Played Songs Data:", highPlayedSongs);
 
   // Process songs/filtered songs
   const processedSongs = highPlayedSongs.map((song) => {
